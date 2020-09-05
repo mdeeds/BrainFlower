273c0c4ed5452b676cfd3914ede8d17db3a9d056
Matt Deeds <mdeeds@gmail.com>
2020-09-05 09:45:35 -0700

    Put cirlcebot's circle in the right place.

--- entries/circleBot.js
+++ entries/circleBot.js
@@ -8,7 +8,9 @@ class CircleBot {
     c.ellipse(50, 50, 95, 95);
     c.stroke(color(0,0,0));
     c.fill(color(255, 255, 200));
-    c.ellipse(80, 35, 20, 20);
-    c.ellipse(80, 35, 15, 15);
+    c.ellipse(50, 20, 20, 20);
+    c.ellipse(50, 20, 15, 15);
+
+    c.rect(40, 40, 20, 20);
   }
 };
