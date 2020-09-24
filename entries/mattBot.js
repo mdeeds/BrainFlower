6a56e002f1cc727d3079f808da6ee7925859f40d
Matt Deeds <mdeeds@gmail.com>
2020-09-23 17:16:55 -0700

    Clean up formatting in MattBot

--- entries/mattBot.js
+++ entries/mattBot.js
@@ -1,25 +1,24 @@
 /**
- * Matt's robot for the BrainFlower tournament.
+ * Matt's entry in the BrainFlower Game
  */
+class MattBot {
+    draw(c) {
+        c.strokeWeight(10);
+        c.stroke(color("PaleTurquoise"));
+        c.fill(color("LightSkyBlue"));
+        c.ellipse(50, 50, 90, 90);
+        c.rect(75, 30, 20, 40);
+    }
 
- class MattBot {
-     draw(c) {
-         c.fill(color("LightBlue"));
-         c.stroke(color("RoyalBlue"));
-         c.strokeWeight(10);
-         c.ellipse(50, 50, 90, 90);
-         c.rect(80, 30, 20, 40);
-     }
-
-     /**
-      * 
-      * @param {SensorState} s 
-      */
-     run(s) {
+    /**
+     * 
+     * @param {SensorState} s 
+     */
+    run(s) {
         if (s.leftFlowers > s.rightFlowers) {
             return 0.3;
         } else {
             return -0.4;
         }
-     }
- }
+    }
+}
