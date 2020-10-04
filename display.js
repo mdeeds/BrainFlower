5a14c9eae3fed298ea3131eff13144f0864c4f99
shinwhai <shinwhai.isla@gmail.com>
2020-10-04 16:15:09 -0700

    hi

--- display.js
+++ display.js
@@ -14,6 +14,7 @@ function setup() {
   entries.push(new Kili());
   entries.push(new SteveBot());
   entries.push(new MattBot());
+  entries.push(new tinyBot());
 
   entries.push(new Mooo());
   let dt = 2 * Math.PI / entries.length;
