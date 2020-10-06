3099d3cbd245530b1e0c384ae30f1e1fc3949e5b
Jason Haase <jasonh@senecagroup.com>
2020-10-06 15:59:42 -0700

    hi

--- entries/Mooo.js
+++ entries/Mooo.js
@@ -27,15 +27,15 @@ class Mooo  {
     if (s.opponentAngle < -60) {
       return -0.8;
     } else if (s.opponentAngle < -10) {
-      return -0.20;
+      return -0.50;
     } else if (s.opponentAngle < -5) {
-      return -0.20;
+      return -0.50;
     } else if (s.opponentAngle < 5) {
-      return 0.20;
+      return 0.50;
     } else if (s.opponentAngle < 10) {
-      return 0.20;
+      return 0.50;
     } else if (s.opponentAngle < 60) {
-      return 0.20;
+      return 0.50;
     } else {
       return -0.10;
     }
